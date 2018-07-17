226c913a0a000c5437f11eff5a33fc968e2eaf57
Your Name <email@example.com>
2018-07-17 21:38:18 +0800

    submit F0

---
+++ F0/1.js
@@ -0,0 +1,12 @@
+	var demoImg = document.getElementById("demo_img");
+	var demoInput = document.getElementById("demo_input");
+    //Òþ²Øtext block£¬ÏÔÊ¾password block
+	function hideShowPsw(){
+		if (demoInput.type == "password") {
+			demoInput.type = "text";
+			demo_img.src = "invisible.png";
+		}else {
+			demoInput.type = "password";
+			demo_img.src = "visible.png";
+		}
+	}
